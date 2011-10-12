c7d1e616daccfe449ad87f060a6473d96ea2e71b
Perry Lei <perry.lei@gmail.com>
2011-10-12 23:18:37 +0800

    update category id by frequency correctly via python

--- WordTotalFrequency/Data Importer/sqlite2coredata.py
+++ WordTotalFrequency/Data Importer/sqlite2coredata.py
@@ -12,27 +12,28 @@ outConn.execute("DELETE FROM ZWORD")
  
 maxId = 0
 count = -1
-inCursor.execute("select * from word")
+inCursor.execute("select * from word order by frequency desc")
+#print inCursor.fetchall()
 for row in inCursor:
-    if row[0] > maxId:
-        maxId = row[0]
- 
- 	count = count + 1
-    # Create ZWORD entry
-    vals = []
-    vals.append(row[0])				# Z_PK
-    vals.append(1)					# Z_ENT
-    vals.append(1)					# Z_OPT
-    vals.append(row[3])				# ZRANK
-    vals.append(0)					# ZMARKED
-    vals.append(min(count/3000, 4)) # ZCATEGORY row[11]
-    vals.append(row[7])				# ZTRANSLATE
-    vals.append(row[8])				# ZTAGS
-    vals.append(row[1])				# ZSPELL
-    vals.append(row[4])				# ZPHONETIC
-    vals.append(row[9])				# ZDETAIL
-    vals.append(row[5])				# ZSOUNDFILE
-    outConn.execute("insert into ZWORD values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", vals)
+	if row[0] > maxId:
+		maxId = row[0]
+	
+	count = count + 1
+	# Create ZWORD entry
+	vals = []
+	vals.append(count+1)			# Z_PK	row[0]
+	vals.append(1)					# Z_ENT
+	vals.append(1)					# Z_OPT
+	vals.append(row[3])				# ZRANK
+	vals.append(0)					# ZMARKED
+	vals.append(min(count/3000, 4)) # ZCATEGORY	row[11]
+	vals.append(row[7])				# ZTRANSLATE
+	vals.append(row[8])				# ZTAGS
+	vals.append(row[1])				# ZSPELL
+	vals.append(row[4])				# ZPHONETIC
+	vals.append(row[9])				# ZDETAIL
+	vals.append(row[5])				# ZSOUNDFILE
+	outConn.execute("insert into ZWORD values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", vals)
     
     
  
