bf0c55bf65c371e8da9e3502a72e9aa586b74dfe
Perry Lei <perry.lei@gmail.com>
2011-10-29 16:27:09 +0800

    update sqlite database

--- WordTotalFrequency/Data Importer/sqlite2coredata.py
+++ WordTotalFrequency/Data Importer/sqlite2coredata.py
@@ -26,7 +26,7 @@ for row in inCursor:
 	vals.append(1)					# Z_OPT
 	vals.append(0)					# ZMARKSTATUS
 	vals.append(row[3])				# ZRANK
-	vals.append(min(count/2700, 4)) # ZCATEGORY	row[11]
+	vals.append(min(count/2500, 4)) # ZCATEGORY	row[11]
 	vals.append(row[7])				# ZTRANSLATE
 	vals.append(row[8])				# ZTAGS
 	vals.append("")                 # ZMARKDATE
